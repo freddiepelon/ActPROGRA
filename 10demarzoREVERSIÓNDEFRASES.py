a3f2b70c5f1ed48cbd0de50d4acda0318df4aa4c
freddiepelon <anahi.rodrigueza@uanl.edu.mx>
2025-03-18 03:44:41 -0600

    Add files via upload

---
+++ 10demarzoREVERSIÓNDEFRASES.py
@@ -0,0 +1,40 @@
+#10demarzo 2025
+# programa que toma una frase y duevuelve la frase con palabras invertidas
+b = input(f"ingresa una frase o texto largo")
+# c = len(b)
+c = len(b)
+d = list(c) 
+e = d[0] 
+print(e) 
+    #i.split() 
+    #print(i.split())
+    
+    # i = la lista de subcadenas con indice i
+    # b = hola soy jamón
+    # f[1] = jamon
+    # f[2] = soy
+    # f[3] = hola
+
+    # para descomponer cada elemento del str 
+    #for a in range(len(i), 0, -1):
+        #i[a] = i.split()
+        #print(i[a]) 
+        
+        
+        
+        
+        
+        
+        
+    
+    
+        
+     
+        
+    
+ #las subcadenas las convertiremos a cadenas
+
+
+#for e in range(c, d, -1):
+    
+        
---
+++ 10marzo2025 ACTIVIDAD PROGRA.py
@@ -0,0 +1,19 @@
+#actividad programación basica
+a = input(f"ingresa un parrafo o un texto largo: " )
+#los conjuntos se representan con set
+
+c = a.split()
+#va crear una lista de subcadenas
+d = len(c)
+
+i = 0 
+for j in range(0, d):
+    j = j + 1
+    if j==d:
+        j = d 
+        print(f"el número de palabras es {j}" ) 
+
+        
+    
+         
+        
